e19288365b588950cc00e814a4ade3fcd35813b0
kapil <kapil@idyllic-software.com>
2015-06-16 21:58:03 +0530

    kapil commits all

--- app/src/main/java/com/amp/syadav/MainActivity.java
+++ app/src/main/java/com/amp/syadav/MainActivity.java
@@ -4,6 +4,7 @@ package com.amp.syadav;
 
 import java.io.IOException;
 import java.util.Locale;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -15,7 +16,14 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import com.amp.helper.Constants;
 import com.amp.helper.Utility;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.twostars.syadav.R;
+
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -24,6 +32,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -43,7 +52,7 @@ public class MainActivity extends Activity
 	Dialog dialog;
 	private String[] HomeMenu ;
 	private String[] colorCodes = {"#d0021b", "#4a90e2","#f5a623",
-			"#bd10e0", "#417505","#d0021b",
+			"#bd10e0","#d0021b",
 			"#4a90e2", "#f5a623","#bd10e0","#d0021b"};
 
 	private Integer[] HomeMenuResouce = {
@@ -51,7 +60,7 @@ public class MainActivity extends Activity
 			R.drawable.ic_9,
 			R.drawable.ic_5,
 			R.drawable.ic_4,
-			R.drawable.ic_8,
+
 			R.drawable.ic_6,
 			R.drawable.ic_7,
 			R.drawable.ic_10,
@@ -60,7 +69,35 @@ public class MainActivity extends Activity
 
 	};
 
-	@Override
+//-----------------------------------------------
+
+    public static final String EXTRA_MESSAGE = "message";
+    public static final String PROPERTY_REG_ID = "registration_id";
+//    private static final String PROPERTY_APP_VERSION = "appVersion";
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
+    /**
+     * Substitute you own sender ID here. This is the project number you got
+     * from the API Console, as described in "Getting Started."
+     */
+    String SENDER_ID = "994936054010";
+
+    /**
+     * Tag used on log messages.
+     */
+    static final String TAG = "GCM Demo";
+
+    GoogleCloudMessaging gcm;
+    AtomicInteger msgId = new AtomicInteger();
+    Context context;
+
+    String regid;
+
+//-----------------------------------------------
+
+
+
+    @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
@@ -70,7 +107,6 @@ public class MainActivity extends Activity
 				getResources().getString(R.string.title_activity_political_career),
 				getResources().getString(R.string.title_activity_samajwadi_party),
 				getResources().getString(R.string.title_activity_events),
-				getResources().getString(R.string.title_activity_media),
 				getResources().getString(R.string.title_activity_video_gallary),
 				getResources().getString(R.string.title_activity_social_media),
 				getResources().getString(R.string.title_activity_vidhan_sabha),
@@ -78,10 +114,29 @@ public class MainActivity extends Activity
 				getResources().getString(R.string.title_activity_contact)};
 
 
-		if(Utility.isNetworkStatusAvialable(getApplicationContext()))
-		{
-			new getUpgradeTask().execute("updates");
-		} 
+
+        // Check device for Play Services APK. If check succeeds, proceed with GCM registration.
+        if (checkPlayServices()) {
+            gcm = GoogleCloudMessaging.getInstance(this);
+            regid = getRegistrationId(ctx);
+
+            if (regid.isEmpty()) {
+                registerInBackground();
+            }
+            else
+            {
+                registerInBackground();
+               // TextView txt_view = (TextView) findViewById(R.id.txt_hellow);
+                Log.i(TAG, "REG_ID : " + regid.toString());
+                //txt_view.setText("Id is " + regid.toString());
+            }
+        } else {
+            Log.i(TAG, "No valid Google Play Services APK found.");
+        }
+
+
+
+
 
 		Gv = (GridView) findViewById(R.id.gridHome);
 		Gv.setAdapter(new MenuGridNewAdaptor(MainActivity.this,
@@ -125,22 +180,22 @@ public class MainActivity extends Activity
 							EventsActivity.class);
 					startActivity(eventsIntent);
 					break;
+//				case 4:
+//					Intent webViewIntent1 = new Intent(getApplicationContext(),
+//							WebViewActivity.class);
+//					webViewIntent1.putExtra("SOCIAL_URL", getResources().getString(R.string.IN_MEDIA_URL));
+//					String actTitle1 =  getResources().getString(R.string.title_activity_media);
+//					webViewIntent1.putExtra("TITLE", actTitle1);
+//					startActivity(webViewIntent1);
+//					break;
 				case 4:
-					Intent webViewIntent1 = new Intent(getApplicationContext(),
-							WebViewActivity.class);
-					webViewIntent1.putExtra("SOCIAL_URL", getResources().getString(R.string.IN_MEDIA_URL));
-					String actTitle1 =  getResources().getString(R.string.title_activity_media);
-					webViewIntent1.putExtra("TITLE", actTitle1);
-					startActivity(webViewIntent1);
-					break;
-				case 5:
 					// Video
 					Intent videoGallaryIntent = new Intent(getApplicationContext(),
 							VideoGallaryActivity.class);
 					startActivity(videoGallaryIntent);
 
 					break;
-				case 6:
+				case 5:
 					// Contact
 					Intent socialMediaIntent = new Intent(getApplicationContext(),
 							SocialMediaActivity.class);
@@ -148,25 +203,27 @@ public class MainActivity extends Activity
 
 					break;
 
-				case 9:
-					// Contact
-					Intent contactIntent = new Intent(getApplicationContext(),
-							ContactActivity.class);
-					startActivity(contactIntent);
+				case 6:
+					// Constituency
+					Intent vidhanIntent = new Intent(getApplicationContext(),
+                            VidhanSabhaActivity.class);
+					startActivity(vidhanIntent);
 
 					break;
 				case 7:
-					// Vidhan Sabha
-					Intent vidhanIntent = new Intent(getApplicationContext(),
-							VidhanSabhaActivity.class);
-					startActivity(vidhanIntent);
+
+ //Department
+                    Intent departmentIntent = new Intent(getApplicationContext(),
+                            DepartmentActivity.class);
+                    startActivity(departmentIntent);
 
 					break;
 				case 8:
 					// Contact
-					Intent departmentIntent = new Intent(getApplicationContext(),
-							DepartmentActivity.class);
-					startActivity(departmentIntent);
+                    // Vidhan Sabha
+                    Intent contactIntent = new Intent(getApplicationContext(),
+                            ContactActivity.class);
+                    startActivity(contactIntent);
 
 					break;
 
@@ -219,8 +276,8 @@ public class MainActivity extends Activity
 			HttpClient httpclient = new DefaultHttpClient();
 			HttpGet httppost;
 			try {
-				httppost = new HttpGet(Constants.UPGRADE_URL + "?current_version="+Utility.getAppVersion(getApplicationContext()));
-
+				httppost = new HttpGet(Constants.UPGRADE_URL + "?current_version="+Utility.getAppVersion(getApplicationContext()) + "&device_id=" + regid);
+                Log.d("DEVICE_ID", httppost.toString());
 				// Execute HTTP Post Request
 				HttpResponse response = httpclient.execute(httppost);
 				String content = EntityUtils.toString(response.getEntity());
@@ -302,4 +359,161 @@ public class MainActivity extends Activity
 		}
 	}
 
+
+
+    //-------------------------------------
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Check device for Play Services APK.
+        checkPlayServices();
+    }
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Stores the registration ID and the app versionCode in the application's
+     * {@code SharedPreferences}.
+     *
+     * @param context application's context.
+     * @param regId registration ID
+     */
+    private void storeRegistrationId(Context context, String regId) {
+        final SharedPreferences prefs = getGcmPreferences(context);
+//        int appVersion = getAppVersion(context);
+        Log.i(TAG, "Saving regId on app version " + regId);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(PROPERTY_REG_ID, regId);
+//        editor.putInt(PROPERTY_APP_VERSION, appVersion);
+        editor.commit();
+    }
+
+    /**
+     * Gets the current registration ID for application on GCM service, if there is one.
+     * <p>
+     * If result is empty, the app needs to register.
+     *
+     * @return registration ID, or empty string if there is no existing
+     *         registration ID.
+     */
+    private String getRegistrationId(Context context) {
+        final SharedPreferences prefs = getGcmPreferences(context);
+        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+        if (registrationId.isEmpty()) {
+            Log.i(TAG, "Registration not found.");
+            return "";
+        }
+        // Check if app was updated; if so, it must clear the registration ID
+        // since the existing regID is not guaranteed to work with the new
+        // app version.
+//        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
+//        int currentVersion = getAppVersion(context);
+//        if (registeredVersion != currentVersion) {
+//            Log.i(TAG, "App version changed.");
+//            return "";
+//        }
+        return registrationId;
+    }
+
+    /**
+     * Registers the application with GCM servers asynchronously.
+     * <p>
+     * Stores the registration ID and the app versionCode in the application's
+     * shared preferences.
+     */
+    private void registerInBackground() {
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                String msg = "";
+                try {
+                    if (gcm == null) {
+                        gcm = GoogleCloudMessaging.getInstance(context);
+                    }
+                    regid = gcm.register(SENDER_ID);
+                    msg = "Device registered, registration ID=" + regid;
+
+                    // You should send the registration ID to your server over HTTP, so it
+                    // can use GCM/HTTP or CCS to send messages to your app.
+                    sendRegistrationIdToBackend();
+                    // For this demo: we don't need to send it because the device will send
+                    // upstream messages to a server that echo back the message using the
+                    // 'from' address in the message.
+                    // Persist the regID - no need to register again.
+                    storeRegistrationId(context, regid);
+                } catch (IOException ex) {
+                    msg = "Error :" + ex.getMessage();
+                    // If there is an error, don't just keep trying to register.
+                    // Require the user to click a button again, or perform
+                    // exponential back-off.
+                }
+                return msg;
+            }
+
+            @Override
+            protected void onPostExecute(String msg) {
+            }
+        }.execute(null, null, null);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    /**
+     * @return Application's version code from the {@code PackageManager}.
+     */
+    private static int getAppVersion(Context context) {
+        try {
+            PackageInfo packageInfo = context.getPackageManager()
+                    .getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            // should never happen
+            throw new RuntimeException("Could not get package name: " + e);
+        }
+    }
+
+    /**
+     * @return Application's {@code SharedPreferences}.
+     */
+    private SharedPreferences getGcmPreferences(Context context) {
+        // This sample app persists the registration ID in shared preferences, but
+        // how you store the regID in your app is up to you.
+        return getSharedPreferences(MainActivity.class.getSimpleName(),
+                Context.MODE_PRIVATE);
+    }
+    /**
+     * Sends the registration ID to your server over HTTP, so it can use GCM/HTTP or CCS to send
+     * messages to your app. Not needed for this demo since the device sends upstream messages
+     * to a server that echoes back the message using the 'from' address in the message.
+     */
+    private void sendRegistrationIdToBackend() {
+        if(Utility.isNetworkStatusAvialable(getApplicationContext()))
+        {
+            new getUpgradeTask().execute("updates");
+        }
+    }
+    //--------------------------------
 }
